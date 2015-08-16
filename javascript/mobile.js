26c57ef7420931512187d8e5dab9822cadb24cfd
Brad Aylsworth <Brad.Aylsworth@gmail.com>
2015-08-15 20:22:28 -0500

    Put in proper code

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -1,70 +1,25 @@
-function Game(){
-    this.solution = new Array(5);
-    for (var i=0; i<5; i++) {
-        while (true) {
-            var candidate = Math.floor(10 * Math.random());
-            if (this.solution.indexOf(candidate) == -1) {
-                this.solution[i] = candidate;
-                break;
-            }
+var stdout = document.getElementById('stdout');
+var history = document.getElementById('history');
+
+newGame = function () {
+    if (document.getElementsByTagName('input')[0].disabled === true) {
+        var coll = document.getElementsByTagName('input');
+        for (var i=0; i<coll.length; i++) {
+            coll[i].disabled = false;
+        }
+        coll = document.getElementsByTagName('button');
+        for (var i=0; i<coll.length; i++) {
+            coll[i].disabled = false;
+        }
+    } else {
+        if (!confirm("Reset game and start a new one?")) {
+            return false;
         }
     }
-    console.log("New number chosen.");
-    this.history = [];
-}
-Game.prototype.play = function() {
-    while(true) {
-        var guess = prompt("Guess: ");
-        var current;
-        try {
-            current = new Attempt(guess);
-        } catch (e) {
-            console.log("Please enter five digits.");
-            continue;
-        }
-        current.response = this.processAttempt(current);
-        console.log(current.response[0] + "-" + current.reponse[1]);
-        self.history[self.history.length] = current;
-        if (current.response.toString() == [5, 5].toString()) {
-            console.log("You win!");
-            break;
-        }
-    }
-};
-Game.prototype.processAttempt = function(attempt) {
-    var response = [0, 0];
-    var current;
-    for (var i=0; i<5; i++) {
-        current = attempt.inputs[i];
-        if (this.solution.indexOf(current) != -1) {
-            response[0]++;
-            if (this.solution.indexOf(current) == i) {
-                response[1]++;
-            }
-        }
-    }
-    return response;
+    
+    stdout.innerHTML += "Game Start!<br />";
+    history.innerHTML += "1 1 1 1 1   2-6 <br />";
+    stdout.innerHTML += "Game continue, or somethingn!<br />";
 };
 
-function Attempt(guess) {
-    var arrayString = guess.replace(this.rematch, this.rereplace);
-    if (!this.retest.test(arrayString)) {
-        throw new Error("Guess parse error.");
-    } 
-    this.inputs = new Array(5);
-    for (var i=0; i<5; i++) {
-        this.inputs[i] = parseInt(arrayString[i]);
-    }
-    this.response = [-1, -1];
-}
-Attempt.prototype.rematch = / *(\d) *(\d) *(\d) *(\d) *(\d) */;
-Attempt.prototype.rereplace = '$1$2$3$4$5';
-Attempt.prototype.retest = /\d\d\d\d\d/;
-Attempt.prototype.toString = function() {
-    var result = "";
-    for (var i=0; i<5; i++) {
-        result += this.inputs[i] + " ";
-    }
-    result += "  " + this.response[0] + "-" + this.response[1];
-    return result;
-};
+document.getElementById('newButton').onclick = function(){newGame();};
