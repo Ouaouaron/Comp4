a4aae3f7dc1790cc3f4f566b393b4beb703a6ee8
Brad Aylsworth <Brad.Aylsworth@gmail.com>
2015-08-15 21:14:26 -0500

    Might have bungled keypad input with some browsers

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -37,12 +37,12 @@ var newGame = function () {
     stdout.write("Game Start!");
     var temp = new Attempt("12345");
     temp.response = [0, 0];
+    history.element.innerHTML += attempt.toString() + "<br>\n";
     history.logattempt(temp);
 };
 
 document.getElementById('newButton').onclick = function(){newGame();};
 document.getElementById('input').onkeypress = function(){
-    alert(event.charCode);
-    //var num = event.charCode;
-    //return (num >= 48 && num <= 57) || (num >= 96 && num <= 105) || num == 8 || num == 13;
+    var num = event.charCode;
+    return (num >= 48 && num <= 57) || num == 8 || num == 13;
 }
