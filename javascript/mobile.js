63e6ef8dff800cfc082e49cfe79adfa10a7291a5
Brad Aylsworth <Brad.Aylsworth@gmail.com>
2015-08-15 20:50:01 -0500

    fix syntax

--- javascript/mobile.js
+++ javascript/mobile.js
@@ -1,22 +1,22 @@
 stdout = {
-    element: document.getElementById('stdout');
+    element: document.getElementById('stdout'),
     write: function(message) {
         stdout.element.innerHTML += message + "<br>\n";
-    }
+    },
     clear: function() {
         stdout.element.innerHTML = "";
     }
-}
+};
 
 history = {
-    element: document.getElementById('history');
+    element: document.getElementById('history'),
     logAttempt: function(attempt) {
         history.element.innerHTML += attempt.toString() + "<br>\n";
-    }
+    },
     clear: function() {
         history.element.innerHTML = "";
     }
-}
+};
 
 var newGame = function () {
     if (document.getElementsByTagName('input')[0].disabled === true) {
